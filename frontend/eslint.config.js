77581cae22c55feca54c12225e22396f0d293c0f
kxzlxv <denys.kozlov@verloom.app>
2026-01-12 16:20:21 +0100

    fix: update eslint
    Signed-off-by: kxzlxv <denys.kozlov@verloom.app>

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,29 +5,29 @@ import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 
 export default tseslint.config(
-  { ignores: ['build/**', '.svelte-kit/**'] },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettier,
-  {
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 2022,
-        sourceType: 'module'
-      }
-    },
-    plugins: {
-      svelte
-    },
-    rules: {
-      // Svelte rules
-      'svelte/require-styles-at-layer': 'error',
-      'svelte/no-at-html-tags': 'error',
+	{ ignores: ['build/**', '.svelte-kit/**'] },
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	prettier,
+	{
+		languageOptions: {
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: 2022,
+				sourceType: 'module'
+			}
+		},
+		plugins: {
+			svelte
+		},
+		rules: {
+			// Svelte rules
+			'svelte/require-styles-at-layer': 'error',
+			'svelte/no-at-html-tags': 'error',
 
-      // JS/TS
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'no-console': 'warn'
-    }
-  }
+			// JS/TS
+			'@typescript-eslint/no-unused-vars': 'warn',
+			'no-console': 'warn'
+		}
+	}
 );
