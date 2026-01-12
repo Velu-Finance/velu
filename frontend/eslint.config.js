d50b0717dc4a193a2b895763a6fb87f68ca47c1e
kxzlxv <denys.kozlov@verloom.app>
2026-01-12 16:19:11 +0100

    bugfix: i dont understand js at all
    Signed-off-by: kxzlxv <denys.kozlov@verloom.app>

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,41 +1,33 @@
-import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
-import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+export default tseslint.config(
+  { ignores: ['build/**', '.svelte-kit/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      svelte
+    },
+    rules: {
+      // Svelte rules
+      'svelte/require-styles-at-layer': 'error',
+      'svelte/no-at-html-tags': 'error',
 
-export default defineConfig(
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
-
-		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
+      // JS/TS
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn'
+    }
+  }
 );
